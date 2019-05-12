15f3f08afe033f4b8d66f0b38a6f83afecfbbb6a
syumai <syumai@gmail.com>
2019-05-12 22:55:07 +0900

    Add str validation

--- usecases/posts.ts
+++ usecases/posts.ts
@@ -1,10 +1,20 @@
 import { Post } from '../domain/post.ts';
 import { PostsRepository } from '../repositories/posts.ts';
 
+const strMax = 500;
+
 export class CreatePost {
   constructor(private repo: PostsRepository) {}
 
   invoke(name: string, body: string): Post {
+    if (!name || name.length > strMax) {
+      // TODO: Add error msg
+      throw 400;
+    }
+    if (!body || body.length > strMax) {
+      // TODO: Add error msg
+      throw 400;
+    }
     return this.repo.createPost(name, body);
   }
 }
