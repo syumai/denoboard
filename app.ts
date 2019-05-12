0c11d3ffd8051452903c07c3eec67e75a5696490
syumai <syumai@gmail.com>
2019-05-12 21:13:28 +0900

    Fix Server handlers

--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
 import { app, get, post } from 'https://denopkg.com/syumai/dinatra/mod.ts';
 import { PostsMemoryRepository } from './repositories/posts.ts';
 import { Server } from './server.ts';
-import { Response } from 'https://denopkg.com/syumai/dinatra/response.ts';
 
 const postsRepo = new PostsMemoryRepository();
 const server = new Server(postsRepo);
@@ -9,11 +8,5 @@ const server = new Server(postsRepo);
 app(
   get('/', () => `<script>window.location = '/posts';</script>`),
   get('/posts', () => server.showPosts()),
-  post('/posts', ({ params }) => {
-    console.log(params);
-    return server.createPost({
-      name: `${params.name}`,
-      body: `${params.body}`,
-    });
-  })
+  post('/posts', ({ params }) => server.createPost(params))
 );
--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import { PostsRepository } from './repositories/posts.ts';
 import { GetPosts, CreatePost } from './usecases/posts.ts';
 import { renderFile } from 'https://deno.land/x/dejs@0.2.0/dejs.ts';
-import { contentType } from 'https://denopkg.com/syumai/dinatra/mod.ts';
 import { Response } from 'https://denopkg.com/syumai/dinatra/response.ts';
+import { Params } from 'https://denopkg.com/syumai/dinatra/handler.ts';
 
 export class Server {
   constructor(private postsRepo: PostsRepository) {}
@@ -15,7 +15,7 @@ export class Server {
     });
   }
 
-  async createPost({ name, body }: { name: string; body: string }) {
+  async createPost({ name, body }: Params): Promise<Response> {
     const createPost = new CreatePost(this.postsRepo);
     createPost.invoke(name, body);
     const getPosts = new GetPosts(this.postsRepo);
